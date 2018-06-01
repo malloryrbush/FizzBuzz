542fcea64164932b1674f44456864942e252b4ed
malloryrbush <malloryrbush@hotmail.com>
2018-05-31 20:29:32 -0400

    completed all code and tests

--- FizzBuzz.Tests/FizzBuzzerTest.cs
+++ FizzBuzz.Tests/FizzBuzzerTest.cs
@@ -20,6 +20,77 @@ namespace FizzBuzz.Tests
 
             Assert.That(result, Is.EqualTo("1"));
         }
+
+        [Test]
+        public void Get_Returns_2_for_2()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(2);
+
+            Assert.That(result, Is.EqualTo("2"));
+        }
+
+        [Test]
+        public void Get_Returns_Fizz_for_3()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(3);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void Get_Returns_Fizz_for_mul_3()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(6);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+
+        [Test]
+        public void Get_Returns_4_for_4()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(4);
+
+            Assert.That(result, Is.EqualTo("4"));
+        }
+
+
+        [Test]
+        public void Get_Returns_Buzz_for_5()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(5);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void Get_Returns_Buzz_for_mul_5()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(10);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void Get_Returns_FizzBuzz_for_mul_3_and_mul_5()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(15);
+
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
     }
 }
-
--- FizzBuzz/FizzBuzzer.cs
+++ FizzBuzz/FizzBuzzer.cs
@@ -5,12 +5,46 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace FizzBuzz
+    //MALLORY BUSH
 {
     public class FizzBuzzer
     {
         public string Get(int num)
         {
-            return "1";
+
+            if (IsMultipleOf15(num))
+            {
+                return "FizzBuzz";
+            }
+
+            if (IsMultipleOf3(num))
+            {
+                return "Fizz";
+            }
+
+            if(IsMultipleOf5(num))
+            {
+                return "Buzz";
+            }
+
+            return num.ToString();
+
         }
+
+        private bool IsMultipleOf3(int numberToCheck)
+        {
+            return numberToCheck % 3 == 0;
+        }
+
+        private bool IsMultipleOf5(int numberToCheck)
+        {
+            return numberToCheck % 5 == 0;
+        }
+
+        private bool IsMultipleOf15(int numberToCheck)
+        {
+            return numberToCheck % 15 == 0;
+        }
+
     }
 }
